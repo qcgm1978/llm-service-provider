ad7b7ab5aca6841d4c049617d55780c8c66a6d6d
qcgm197874@gmail.com <qcgm197874@gmail.com>
2025-09-30 21:49:34 +0800

    Bump version to 3.1.1

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,29 @@
-// 导出服务提供商枚举
+import * as llmCore from '@qcgm1978/llm-core';
+import initServices from './initServices';
 
-export { ServiceProvider } from './llmService';
+initServices();
 
-// 导出主要功能函数
-export { streamDefinition, generatePrompt } from './llmService';
+export const { 
+  ServiceProvider, 
+  getSelectedServiceProvider, 
+  setSelectedServiceProvider, 
+  hasApiKey, 
+  streamDefinition, 
+  streamMindMap, 
+  streamMindMapArrows, 
+  generatePrompt,
+  getPromptByName,
+  formatPrompt,
+  getPromptsByLanguage,
+  updatePrompt,
+  resetPrompts,
+  getChapterMindMapPrompt,
+  getMindMapArrowPrompt
+} = llmCore;
 
-// 导出思维导图相关功能
-export { streamMindMap, streamMindMapArrows } from './llmService';
+export const init = initServices;
 
-// 导出 API Key 管理功能
-export { hasApiKey, getSelectedServiceProvider, setSelectedServiceProvider,setHasShownApiKeyPrompt } from './llmService';
+export { setHasShownApiKeyPrompt } from './llmService';
 
-// 导出提示管理功能
-export { getPromptByName, formatPrompt, getPromptsByLanguage, updatePrompt, resetPrompts } from './prompts';
-
-export { getChapterMindMapPrompt, getMindMapArrowPrompt } from './mindmap';
-
-// 导出组件
 export { default as ApiKeyManager } from './ApiKeyManager';
 export { default as MindMapVisualizer } from './MindMapVisualizer';
